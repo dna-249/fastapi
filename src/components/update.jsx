93ff87e7fb0cf39060bbc514221081a34b5f26a1
nuraalhaji <danamonuraa@gmail.com>
2025-02-19 17:17:49 +0100

    commit

--- src/components/update.jsx
+++ src/components/update.jsx
@@ -10,23 +10,22 @@ const Update = () => {
     const [item6,setItem6] = useState()
     const [item7,setItem7] = useState()
     const [item,setItem] = useState()
+    const [image,setImage] = useState()
     
       
-    const reader = (file) => {
-      const readUrl = new FileReader()
-       readUrl.readAsDataURL(file)
-      readUrl.onloadend =()=>{
-        setItem(readUrl.result)
-      }
-    }
+    
     const form = new FormData()
     form.append("file",item1)
+  const name = (e) => {
+    setItem1(e.target.files[0])
+    setImage(URL.createObjectURL(e.target.files[0]))
+  }
   
   
   const handleRequest = () => {
-    reader(item1)
+   
     
-
+    axios.post("https://upload-api-yzgr.onrender.com",form).then(()=>alert("all is ok")).catch((err)=>alert(err))
 
     axios.post(`https://server-api-1d7r.vercel.app/api/users`,{image:`http://localhost:3000/file/${item1.name}`,
                                                   name:item2,
@@ -48,9 +47,9 @@ useEffect(() => {
   return (
     <div>
         <div className="input"> 
-          <div style={{height:"200px",width:"200px",backgroundColor: "rgba(0,0,0,0.5)"}}><img src={item}  width={200} alt='' /></div> 
-           <h5> upload Item </h5>
-       Image: <input required type='file'  onChange={(e)=>setItem1(e.target.files[0])} /> <br/>
+          <div style={{height:"200px",width:"200px",backgroundColor: "rgba(0,0,0,0.5)"}}><img src={image}  width={200} alt='' /></div> 
+           <h5 onChange={(e)=>name(e)}> upload Item </h5>
+       Image: <input required type='file'  onChange={(e)=>name(e)} /> <br/>
        Name: <input required type='text'    onChange={(e)=>setItem2(e.target.value)} /> <br/>
        Description:<br/> <textarea  onChange={(e)=>setItem3(e.target.value)} /> <br/>
        Price: <input required type='number' onChange={(e)=>setItem4(e.target.value)} /> <br/>

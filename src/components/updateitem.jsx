fa403031c66261bbd4eb11b874cd99843c2d79a5
nuraalhaji <danamonuraa@gmail.com>
2025-02-22 13:40:00 +0100

    commit

--- src/components/updateitem.jsx
+++ src/components/updateitem.jsx
@@ -1,6 +1,7 @@
 
 import React, { useState,useEffect } from 'react'
 import axios from 'axios'
+import {Link}  fron "react-router-dom"
 
 const UpdateItem = () => {
     const [search,setSearch] = useState('')

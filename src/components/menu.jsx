3f1830b61616ffb17079c3831728e4fea5af5ae5
nuraalhaji <danamonuraa@gmail.com>
2025-02-23 10:01:25 +0100

    commit

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -6,10 +6,13 @@ import { Link } from 'react-router-dom'
 
 const Menu = () => {
   return (
-    <div className='auto'>
-    <div><Link to={'/update'}>New Item</Link></div>
-    <div><Link to={'/updateitem'}>Update Item</Link></div>
-    <div> <Link to={'/delete'}>Delete Item</Link></div>
+    <div className='white auto'>
+      <div className=" auto">
+         <h3>Select option</h3>
+         <div><Link to={'/update'}>New Item</Link></div>
+         <div><Link to={'/updateitem'}>Update Item</Link></div>
+         <div> <Link to={'/delete'}>Delete Item</Link></div>
+      </div>
     </div>
   )
 }

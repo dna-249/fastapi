49a54c194bb1766509ed016231d0f2cc20b56029
nuraalhaji <danamonuraa@gmail.com>
2025-02-23 18:24:16 +0100

    commit

--- src/components/updateitem.jsx
+++ src/components/updateitem.jsx
@@ -41,7 +41,7 @@ const UpdateItem = () => {
 
   const handleUpdateItem = (params) => {
     axios.put(`https://server-api-1d7r.vercel.app/api/users/${select._id}`,
-        {image:`https://upload-api-yzgr.onrender.com/file/${item1.name || null}}`,
+        {image:`https://upload-api-yzgr.onrender.com/file/${item1?.name}}`,
                                                      name:item2,
                                                      description:item3,
                                                      price:`$${item4}`,

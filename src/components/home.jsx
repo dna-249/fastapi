f93357e9b7ca9e4bec971838e6df440337b2ac16
nuraalhaji <danamonuraa@gmail.com>
2025-04-01 15:40:05 +0100

    commit

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -14,7 +14,7 @@ const Home = ({data})=>{
             {data
             .map((item,index)=> <div key={index} onClick={()=>setShow(()=>false)}>
 
-           <div onClick={()=>setSelect(item)} > <img className='img2' src={item.image} loading='lazy' alt=""  /></div>
+           <div onClick={()=>setSelect(item)} > <img className='img2' src={item.image}  alt=""  /></div>
             </div>)}
           </div>
         ):(<div className='auto'><Item data={select}  /></div> )}
--- src/components/order.jsx
+++ src/components/order.jsx
@@ -4,7 +4,7 @@ const Order=({data})=>{
   return (
     <>
     <div className='auto'>
-    <div><img loading='lazy' src={data.image} width={200} height={250} alt=''/></div>
+    <div><img src={data.image} width={200} height={250} alt=''/></div>
     Item Info
         <div>Name:{data.name}</div> 
         <div>Category:{data.category}</div>
